145992610ae29bee2d8043806cb2fc3341f60c28
okoloboga <okoloboga37@gmail.com>
2025-05-01 09:28:44 +0300

    removed golang backend - use only fastapi

--- backend_old/pkg/payment/provider.go
+++
@@ -1,7 +0,0 @@
-package payment
-
-// PaymentProvider представляет интерфейс для платежных систем
-type PaymentProvider interface {
-	InitiatePayment(userID int, amount float64) (string, error)
-	ProcessWebhook(payload []byte) (int, float64, string, error)
-}
